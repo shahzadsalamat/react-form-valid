0b62cdd5ba12e4ef902ebe4858d1b83e58f3d802
shahzadsalamat <shahzad.salamat@yahoo.com>
2020-04-16 02:45:06 +0300

    fixed the bugs

--- src/components/userForm.js
+++ src/components/userForm.js
@@ -1,25 +1,22 @@
 import React, { Component } from 'react';
 
-const emailValidation = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const emailValidation = /^([A-Za-z0-9_\-.])+@([A-Za-z0-9_\-.])+\.([A-Za-z]{2,4})$/;
 
-const formValidation = ({errorMessages, ...rest}) => {
-      let valid = true;
-     const aray = Object.values(errorMessages);
-      aray.forEach(val => {
-        val.length > 0 &&  (valid = false);
-      });
-
-      const array = Object.values(rest);
-      array.forEach(val => {
-        val.length === 0 &&  (valid = false);
-      });
-      return valid;
-
-
-      
-     
+const formValidation = ({ errorMessages, ...rest }) => {
+  let valid = true;
+  //check if any input is invalid
+  const aray = Object.values(errorMessages);
+  aray.forEach(val => {
+    val.length > 0 && (valid = false);
+  });
+  //check if any input is empty
+  const array = Object.values(rest);
+  array.forEach(val => {
+    val.length === 0 && (valid = false);
+  });
+  return valid;
 };
- 
+
 class Userform extends Component {
   constructor(props) {
     super(props)
@@ -35,33 +32,35 @@ class Userform extends Component {
         password: '',
       }
     }
+    this.initialState = this.state;
   }
 
   handleSubmit = (e) => {
     e.preventDefault();
     if (formValidation(this.state)) {
-      console.log('form is valid');
+      alert('Form is Valid');
+      //change state back to initial state
+      this.setState(this.initialState);
     }
     else {
-      console.log('form is invalid');
-
+      alert('Form is not valid');
     }
   }
 
   handleChange = (e) => {
     e.preventDefault();
-   
-    const {name, value} = e.target
+
+    const { name, value } = e.target;
     let errorMessages = this.state.errorMessages;
     switch (name) {
       case 'firstName':
         errorMessages.firstName = value.length < 3 && value.length > 0
-          ? 'first name must be more than 3 letters'
+          ? 'minimum 3 letters required'
           : ''
         break;
       case 'lastName':
         errorMessages.lastName = value.length < 3 && value.length > 0
-          ? 'last name must be more than 3 letters'
+          ? 'minimum 3 letters required'
           : ''
         break;
       case 'email':
@@ -70,68 +69,72 @@ class Userform extends Component {
           : 'email is invalid'
         break;
       case 'password':
-          errorMessages.password = value.length < 6 && value.length > 0
-            ? 'Enter minimum 6 letters'
-            : ''
-          break;
-          default:
-            break;
+        errorMessages.password = value.length < 6 && value.length > 0
+          ? 'minimum 6 letters required'
+          : ''
+        break;
+      default:
+        break;
     }
     this.setState({
-      [e.target.name]: e.target.value,
-      errorMessages
+      errorMessages,
+      [name]: value
     });
   };
   render() {
-
+    const { errorMessages } = this.state;
     return (
       <div>
         <form className='form-wrapper' onSubmit={this.handleSubmit}>
           <div className='firstname'>
             <label>Firstname</label>
             <input
+              className={errorMessages.firstName.length > 0 ? 'errorMessage-highlight' : ''}
               type='text'
               placeholder='firstname'
               name='firstName'
               value={this.state.firstName}
               onChange={this.handleChange}
             />
-            <span className='errorMessage'>{this.state.errorMessages.firstName}</span>
+            <span className='errorMessage'>{errorMessages.firstName}</span>
           </div>
           <div className='lastname'>
             <label>Lastname</label>
             <input
+              className={errorMessages.lastName.length > 0 ? 'errorMessage-highlight' : ''}
               type='text'
               placeholder='lastname'
               name='lastName'
               value={this.state.lastName}
               onChange={this.handleChange}
             />
-            <span className='errorMessage'>{this.state.errorMessages.lastName}</span>
+            <span className='errorMessage'>{errorMessages.lastName}</span>
 
           </div>
           <div className='email'>
             <label>Email</label>
             <input
+              className={errorMessages.email.length > 0 ? 'errorMessage-highlight' : ''}
               type='email'
               placeholder='email'
               name='email'
               value={this.state.email}
               onChange={this.handleChange}
             />
-            <span className='errorMessage'>{this.state.errorMessages.email}</span>
+            <span className='errorMessage'>{errorMessages.email}</span>
 
           </div>
           <div className='password'>
             <label>password</label>
             <input
+              className={errorMessages.password.length > 0 ? 'errorMessage-highlight' : ''}
               type='password'
               placeholder='firstname'
               name='password'
               value={this.state.password}
               onChange={this.handleChange}
             />
-            <span className='errorMessage'>{this.state.errorMessages.password}</span>
+            <span className='errorMessage'>{errorMessages.password}</span>
           </div>
           <div className='button'>
             <button>Submit</button>
